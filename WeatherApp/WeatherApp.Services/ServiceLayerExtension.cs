fbdbbff705cd05827b97cc6f225eeaa1feec904c
James Tubiano <tubianojames@gmail.com>
2020-01-30 22:56:45 +0800

    project init

---
+++ WeatherApp/WeatherApp.Services/ServiceLayerExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WeatherApp.Services
+{
+    public static class ServiceLayerExtension
+    {
+        public static IServiceCollection RegisterService(this IServiceCollection service)
+        {
+
+        }
+    }
+}
